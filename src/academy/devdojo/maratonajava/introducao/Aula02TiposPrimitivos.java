ecb340cd7dc86d9ccb9205e6c88fa076fb60ecf2
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-07-09 16:40:58 -0300

    :rocket: nova feature - Tipos primitivos pt 03 - casting

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -17,5 +17,21 @@ public class Aula02TiposPrimitivos {
         System.out.println("A idade é "+idade+" anos");
         System.out.println(falso);
         System.out.println("char "+caractere);
+
+        // casting
+        int idade1 = (int) 10000000000L;
+        long numeroGrande1 = (long) 155.23;
+        double salarioDouble1 = 2000.0D;
+        float salarioFloat1 = (float) 2500.0D;
+        byte idadeByte1 = 127;
+        short idadeShort1 = 32000;
+        boolean verdadeiro1 = true;
+        boolean falso1 = false;
+        char caractere1 = '\u0041';
+
+        System.out.println("A idade é "+idade1+" anos");
+        System.out.println(falso1);
+        System.out.println("char "+caractere1);
+        System.out.println(numeroGrande1);
     }
 }
