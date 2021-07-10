2585cbf09baa28559d18b8960e42c6653c4a2f3d
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-07-10 11:27:22 -0300

    :rocket: nova feature - Operadores pt 02 - Relacionais

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula04Operadores {
 
     public static void main(String[] args) {
+        // operadores aritimeticos
         // + - / *
         int numero01 = 10;
         int numero02 = 20;
@@ -15,5 +16,23 @@ public class Aula04Operadores {
         System.out.println(multiplicar);
         System.out.println(subtrair);
         System.out.println(dividir);
+
+        //operadores relacionais
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10.0;
+        boolean isDezDiferenteDez = 10 != 10.0;
+
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
     }
 }
