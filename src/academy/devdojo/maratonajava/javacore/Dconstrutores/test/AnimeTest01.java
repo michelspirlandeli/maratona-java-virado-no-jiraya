898316f9abc6a65aa5d8552ff5a48598f342b246
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-07-31 16:08:10 -0300

    :rocket:nova feature -  Orientação Objetos - Construtores pt 02

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,9 +4,12 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
+//        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
+//        Anime anime2 = new Anime();
+//        anime.imprime();
+//        anime2.imprime();
+
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação", "Production IG");
         anime.imprime();
-        anime2.imprime();
     }
 }
