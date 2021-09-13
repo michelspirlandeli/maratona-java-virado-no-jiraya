69bd6aeae69b9f4dcc86aa98f298b03fa6406d84
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-09-13 13:59:30 -0300

    :rocket:nova feature - Classes Utilitárias - Strings pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest02 {
+    public static void main(String[] args) {
+        String nome = "Luffy";
+        String numeros = "012345";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("f","l"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+        System.out.println(numeros.length());
+        System.out.println(numeros.substring(3,numeros.length()));
+        System.out.println(nome.trim());
+    }
+}
