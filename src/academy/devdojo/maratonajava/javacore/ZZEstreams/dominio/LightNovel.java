2e29114168ac327d0b3b0682911f30a6998f3bfe
michelspirlandeli <michelspirlandeli10@gmail.com>
2022-05-27 11:03:55 -0300

    :rocket:nova feature - Streams pt 12 - Collectors pt 02 - Grouping by pt 01

--- src/academy/devdojo/maratonajava/javacore/ZZEstreams/dominio/LightNovel.java
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/dominio/LightNovel.java
@@ -1,12 +1,34 @@
 package academy.devdojo.maratonajava.javacore.ZZEstreams.dominio;
 
+import java.util.Objects;
+
 public class LightNovel {
     private String title;
     private double price;
+    private Category category;
 
     public LightNovel(String title, double price) {
         this.title = title;
         this.price = price;
+    }
+
+    public LightNovel(String title, double price, Category category) {
+        this.title = title;
+        this.price = price;
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LightNovel that = (LightNovel) o;
+        return Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
     }
 
     @Override
@@ -14,7 +36,12 @@ public class LightNovel {
         return "LightNovel{" +
                 "title='" + title + '\'' +
                 ", price=" + price +
+                ", category=" + category +
                 '}';
+    }
+
+    public Category getCategory() {
+        return category;
     }
 
     public String getTitle() {
