eef5caac9e796a71be1eacc0d309ef8883e07d14
michelspirlandeli <michelspirlandeli10@gmail.com>
2022-06-09 14:41:23 -0300

    :rocket:nova feature - JDBC pt 08 - Deletando dados com Statement

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,13 +1,16 @@
-package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
+package main.java.academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
 import lombok.extern.log4j.Log4j2;
+import main.java.academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
 
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
-        ProducerRepository.save(producer);
+//        ProducerService.save(producer);
+        ProducerService.delete(5);
+
     }
+
 }
