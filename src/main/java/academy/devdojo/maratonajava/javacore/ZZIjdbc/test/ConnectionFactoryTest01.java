33e9cc11fc134c2dff99d3ee8fc04e8a13e4e3f4
michelspirlandeli <michelspirlandeli10@gmail.com>
2022-07-30 17:45:30 -0300

    :rocket:nova feature - JDBC pt 09 - Atualizando dados com Statement

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -8,8 +8,10 @@ import main.java.academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerS
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
+        Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
 //        ProducerService.save(producer);
-        ProducerService.delete(5);
+//        ProducerService.delete(5);
+        ProducerService.update(producerToUpdate);
 
     }
 
